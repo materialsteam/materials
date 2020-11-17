fd8d16ec8f5944da0b89ef8354cfc2d7cb4ad27c
troy mokoagow <troymokoagow86@gmail.com>
2020-11-17 17:22:20 +0800

    create splashscreen and design navigation homescreen

---
+++ app/src/main/java/com/materialsteam/materials/BottomNavigation.java
@@ -0,0 +1,54 @@
+package com.materialsteam.materials;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.materialsteam.materials.fragments.AkunFragment;
+import com.materialsteam.materials.fragments.HomeFragment;
+import com.materialsteam.materials.fragments.TukangFragment;
+
+public class BottomNavigation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+
+        loadFragment(new HomeFragment());
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null){
+            getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frame, fragment)
+                .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()){
+            case R.id.home_menu:
+                fragment = new HomeFragment();
+                break;
+            case R.id.tukang_menu:
+                fragment = new TukangFragment();
+                break;
+            case R.id.akun_menu:
+                fragment = new AkunFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+}
